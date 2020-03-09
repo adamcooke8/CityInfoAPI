cdda3e9007ff297edcc19ae94b5996607e285f46
Adam Cooke <Adam.Cooke@fenergo.com>
2020-03-09 00:35:09 +0000

    Add project files.

---
+++ CityInfoAPI/Controllers/CitiesController.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using CityInfo.API;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfoAPI.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            // find city
+            var cityToReturn = CitiesDataStore.Current.Cities
+                .FirstOrDefault(c => c.Id == id);
+
+            if (cityToReturn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cityToReturn);
+        }
+    }
+}
